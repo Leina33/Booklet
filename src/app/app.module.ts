0b68ea318e4dcda35070ae77b55c14bcdded2e2f
Leina33 <dominickrutto@gmail.com>
2019-11-05 10:00:07 +0300

    Created a about compenent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
+    AboutComponent,
      
   ],
   imports: [
